6775af0e6f710a6dc49297374bb9e5d0c3701d5b
abtam <69460011+abtam@users.noreply.github.com>
2020-10-07 17:07:14 +0530

    Add files via upload

---
+++ GDP Share Analysis.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('fivethirtyeight')
+
+GDP_share_pct=pd.read_csv('GDP Share in Percent.csv',index_col=0)
+
+print(GDP_share_pct)
+
+sectors=np.array(GDP_share_pct.columns)
+year=np.array(GDP_share_pct.index)
+
+plt.plot(year,GDP_share_pct.iloc[:,0:])
+plt.xticks(rotation=90)
+
+plt.xlabel('Year')
+plt.ylabel('Percentage of GDP')
+plt.legend(sectors,prop={'size':10})
+plt.tight_layout()
+
+plt.show()
